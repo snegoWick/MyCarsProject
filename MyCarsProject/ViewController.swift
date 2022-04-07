4d99a14ed01aa59aa7c270403f6d0ba4c0361fe2
snegoWick <quillmcfly@gmail.com>
2022-04-07 14:21:12 +0400

    Initial Commit

---
+++ MyCarsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCarsProject
+//
+//  Created by Aleksandr Makarov on 07.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
